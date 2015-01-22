e1d4bee75c23536370cb8afedda42f6207518f04
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-01-21 19:33:32 -0800

    made first name required. added some HTML and formatting - bootstrap

--- userapp/djangousers/models.py
+++ userapp/djangousers/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Contact(models.Model):
 
-    first_name = models.CharField(max_length=255,)
+    first_name = models.CharField(max_length=255, blank=False)
     last_name = models.CharField(max_length=255,)
-    email = models.EmailField()
+    email = models.EmailField(max_length=254,)
 
     def __str__(self):
         return ' '.join([self.first_name, self.last_name])
