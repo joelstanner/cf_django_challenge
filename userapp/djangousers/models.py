4443577951617053fbd2c28d05eee5a253d7d8fc
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-01-22 00:37:38 -0800

    edit buttons smaller, last_name and email not required

--- userapp/djangousers/models.py
+++ userapp/djangousers/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Contact(models.Model):
 
     first_name = models.CharField(max_length=255, blank=False,)
-    last_name = models.CharField(max_length=255,)
-    email = models.EmailField(max_length=254,)
+    last_name = models.CharField(max_length=255, blank=True,)
+    email = models.EmailField(max_length=254, blank=True,)
 
     def __str__(self):
         return ' '.join([self.first_name, self.last_name])
